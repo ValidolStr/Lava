8c4a5ce79d422f6340877ce059ac0685d04b26b4
ValidolStr <chizhewskyvadim@gmail.com>
2024-11-24 19:31:24 +0300

    testingvariant

--- app/src/main/java/com/example/application02/TestForm.java
+++ app/src/main/java/com/example/application02/TestForm.java
@@ -1,6 +1,7 @@
 package com.example.application02;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 
@@ -8,32 +9,58 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 
 public class TestForm extends AppCompatActivity {
+
+    private FrameLayout frameLayout;
+    private fiveresult fiver = new fiveresult();
+    private fourresult fourr = new fourresult();
+    private  sickbar sick = new sickbar();
+    private yesornot yn = new yesornot();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_test_form);
-
-        setContentView(R.layout.activity_test_form);
-
-//        // Получаем индекс из Intent
-//        int itemIndex = getIntent().getIntExtra("item_index", -1); // -1 — значение по умолчанию, если данные не передались
-//
-//        // Используем индекс (например, отображаем его в TextView или используем для логики)
-//        TextView textView = findViewById(R.id.textView2);
-//        textView.setText("Выбранный индекс: " + itemIndex);
-
-
-
+        init();
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void init (){
+        int itemIndex = getIntent().getIntExtra("item_index", 0);
+        frameLayout = findViewById(R.id.frameLayout);
+        switch (itemIndex) {
+            case 0:
+                setnewfragment(yn);
+                break;
+
+            case 1:
+                setnewfragment(fiver);
+                break;
+
+            case 2:
+                setnewfragment(fourr);
+                break;
+
+            case 3:
+                setnewfragment(sick);
+                break;
+
+            default:
+                throw new IllegalArgumentException("Неизвестный itemIndex: " + itemIndex);
+        }
+
+    }
+    private void setnewfragment (Fragment fragment){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.frameLayout, fragment);
+        ft.commit();
     }
 }
